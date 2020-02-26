e8355ab94789a7cacd3b5696d8e918294807df6f
Martin Števko <mstevko10@gmail.com>
2020-02-26 12:07:35 +0100

    Update views.py

--- finances/views.py
+++ finances/views.py
@@ -18,11 +18,16 @@ class BalanceView(ListView):
             temp = []
             types = TransactionType.objects.filter(section=section[0])
             for t in types:
+                c = t.ammount
                 approvals = Approval.objects.filter(transaction_type=t)
                 s = 0
                 for a in approvals:
                     if a.transaction.state in ['payed', 'public']:
                         s += a.transaction.ammount
-                temp.append((t.name, s))
+                extras = ExtraExpense.objects.filter(transaction_type=t)
+                for a in extras:
+                    if a.transaction.state in ['payed', 'borrowed']:
+                        s += a.transaction.ammount
+                temp.append((t.name, s, c))
             context['data'].append((section[1], temp))
         return context
